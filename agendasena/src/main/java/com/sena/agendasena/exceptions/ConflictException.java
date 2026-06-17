682d4803206352d4ab8c983263fb02a70ccb5047
Ferrer007 <ferrercastillo2005@gmail.com0>
2026-06-17 14:37:37 -0500

    Exception: Creación de excepciones personalizadas para Bad Request (400) y Conflict (409)

---
+++ agendasena/src/main/java/com/sena/agendasena/exceptions/ConflictException.java
@@ -0,0 +1,11 @@
+package com.sena.agendasena.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ConflictException extends RuntimeException {
+    public ConflictException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ agendasena/src/main/java/com/sena/agendasena/exceptions/ReglaNegocioException.java
@@ -0,0 +1,11 @@
+package com.sena.agendasena.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ReglaNegocioException extends RuntimeException {
+    public ReglaNegocioException(String mensaje) {
+        super(mensaje);
+    }
+}
